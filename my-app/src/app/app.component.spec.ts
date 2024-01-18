a671395c66921ca5a92ba46af2350948fc582a3c
Verhus <atanaszaharieff@abv.bg>
2024-01-18 17:03:33 +0200

    Testing app and login.

--- my-app/src/app/app.component.spec.ts
+++ my-app/src/app/app.component.spec.ts
@@ -1,29 +1,41 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  let component:AppComponent;
+  let fixture:ComponentFixture<AppComponent>
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers:[],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+      schemas:[CUSTOM_ELEMENTS_SCHEMA]
+    });
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges()
   });
 
-  it(`should have as title 'my-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-app');
+  it('should create the app', () => {    
+    expect(component).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-app app is running!');
-  });
+  it('should have header', () => {
+    const headerElement = fixture.nativeElement.querySelector('header')    
+    expect(headerElement).toBeTruthy();
+  })
+
+  it('should have navigation', () => {
+    const navigation = fixture.nativeElement.querySelector('app-navigation')        
+    expect(navigation).toBeTruthy();
+  })
+
+  it('should have main', () => {
+    const main = fixture.nativeElement.querySelector('main')    
+    expect(main).toBeTruthy();
+  })
+
 });
--- my-app/src/app/auth/login/login.component.spec.ts
+++ my-app/src/app/auth/login/login.component.spec.ts
@@ -1,15 +1,54 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
 import { LoginComponent } from './login.component';
+import { HttpClient } from '@angular/common/http';
+import { NO_ERRORS_SCHEMA, NgModule } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { AuthService } from 'src/app/services/auth.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+import { Router } from '@angular/router';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let authService: jasmine.SpyObj<AuthService>;
+  let mockAuthService: any;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
+  @NgModule()
+  class FixNavigationTriggeredOutsideAngularZoneNgModule {
+    constructor(_router: Router) {
+    }
+  }
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [LoginComponent]
+
+      declarations: [LoginComponent],
+
+      imports: [
+        HttpClientTestingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        FixNavigationTriggeredOutsideAngularZoneNgModule
+      ],
+
+      schemas: [NO_ERRORS_SCHEMA],
+
+      providers: [
+        { provide: AuthService, useValue: mockAuthService }
+      ]
     });
+
+
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+
+    mockAuthService = jasmine.createSpyObj('AuthService', ['login']);
+
+    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>
+
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -18,4 +57,95 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+
+  it('should have form', () => {
+    const form = fixture.nativeElement.querySelector('form');
+    expect(form).toBeTruthy()
+  })
+
+  it('should have email input inside form', () => {
+    const email = fixture.nativeElement.querySelectorAll('input')[0];
+    expect(email).toBeTruthy()
+  })
+
+  it('should have password input inside form', () => {
+    const password = fixture.nativeElement.querySelectorAll('input')[1];
+    expect(password).toBeTruthy()
+  })
+
+  it('should have submit input inside form', () => {
+    const button = fixture.nativeElement.querySelectorAll('input')[2];
+    expect(button).toBeTruthy()
+  })
+
+  it('should  change email and password values', fakeAsync(() => {
+    const email: HTMLInputElement = fixture.nativeElement.querySelectorAll('input')[0];
+    const password: HTMLInputElement = fixture.nativeElement.querySelectorAll('input')[1];
+    const emailValue = 'peter@abv.bg'
+    const passwordValue = '123456'
+
+    email.value = emailValue;
+    password.value = passwordValue;
+
+    email.dispatchEvent(new Event('input'))
+    password.dispatchEvent(new Event('input'))
+
+    tick()
+    fixture.detectChanges();
+
+    expect(email.value).toEqual(emailValue)
+    expect(password.value).toEqual(passwordValue)
+  }))
+
+  it('should  call onSubmit ', fakeAsync(() => {
+    const form = fixture.debugElement.query(By.css('form'))
+    const func = spyOn(component, 'onSubmit');
+
+    fixture.detectChanges();
+
+    form.triggerEventHandler('ngSubmit', null)
+    tick()
+    fixture.detectChanges();
+
+    expect(func).toHaveBeenCalled();
+  }))
+
+  it('should call login when form event fire', fakeAsync(() => {
+    const form = fixture.debugElement.query(By.css('form'))
+    const email: HTMLInputElement = fixture.nativeElement.querySelectorAll('input')[0];
+    const password: HTMLInputElement = fixture.nativeElement.querySelectorAll('input')[1];
+    const emailValue = 'peter@abv.bg'
+    const passwordValue = '123456'
+    
+    authService.login.and.returnValue(of(true))
+
+    fixture.detectChanges()
+
+    email.value = emailValue;
+    password.value = passwordValue;
+
+    email.dispatchEvent(new Event('input'))
+    password.dispatchEvent(new Event('input'))
+
+    tick()
+    form.triggerEventHandler('ngSubmit', null)
+    tick()
+    fixture.detectChanges()
+
+    expect(authService.login).toHaveBeenCalled()
+  }))
+
+  it('should not to call login when form event fire', fakeAsync(() => {
+    const form = fixture.debugElement.query(By.css('form'))    
+    authService.login.and.returnValue(of(true))
+
+    tick()
+    form.triggerEventHandler('ngSubmit', null)
+    tick()
+    fixture.detectChanges()
+
+    expect(authService.login).not.toHaveBeenCalled()
+  }))
+
+
+});//end
